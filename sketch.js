bb2a8853e575127b16e8da1e2350d34f557c8061
PriyaPethuraj <77150619+PriyaPethuraj@users.noreply.github.com>
2021-06-13 14:02:25 +0530

    Update sketch.js
    I have changed the function keyPressed()

--- sketch.js
+++ sketch.js
@@ -80,35 +80,27 @@ function draw() {
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
 
-  if(keyCode === LEFT_ARROW) {
-	helicopterSprite.x = helicopterSprite.x-20 ;
-	Matter.Body.translate(packageBody, {x:-20, y:0}) 
-   }
- 
- 
-   if(keyCode === RIGHT_ARROW) {
-	 helicopterSprite.x = helicopterSprite.x+20 ;
-	 Matter.Body.translate(packageBody, {x:+20, y:0})
-	}
- 
-   if(keyCode === DOWN_ARROW) {
-	 Matter.Body.setStatic(packageBody, false);
-	 if(packageBody.position.y > 400){
-		packageBody.setVelocity = 0;
-		gamestate = END;
-	}
-	
-	
- 
-
-	
+    drawSprites();
   
+  
+ 
 }
 
-	
-   
-  drawSprites();
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+
+    helicopterSprite.x=helicopterSprite.x-20;    
+    translation={x:-20,y:0}
+    Matter.Body.translate(packageBody, translation)
+
+
+  } else if (keyCode === RIGHT_ARROW) {
+    helicopterSprite.x=helicopterSprite.x+20;
+    translation={x:20,y:0}
+    Matter.Body.translate(packageBody, translation)
+  }
+  else if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+    
+  }
 }
-  
-
-
